e6a0465e6719ff32e9e487be20875dc097c6b918
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2025-07-12 19:18:13 +0000

    datatypes

---
+++ basics/data_types.js
@@ -0,0 +1,21 @@
+"use strict" // use java as newer version
+//alert (3+3) // we are using node js not browser.. google
+
+console.log(3+3);
+console.log("mee")
+
+let name ="me"
+let age = 34
+let boolean = true
+
+// number => 2 to power 53
+// bigint
+// boolean => true, false
+// null => standalone value
+// undefined =>
+    //symbols=> uniqueness
+// object
+
+console.log(typeof 34)
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12234
+let accountEmail = "maryam@gamil.com"
+var accountPassword ="1234"
+accountCity = "Lahore"
+let accountState;
+// accountId =88997
+accountEmail = "asad"
+accountPassword = "5677"
+accountCity="Karachi"
+console.log(accountId); 
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
